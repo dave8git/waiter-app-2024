5fa09b2b831a375e50781201ff910894d73dec41
dave8git <dave8git@gmail.com>
2024-08-25 23:19:52 +0200

    Add json-server configuration added

---
+++ src/redux/initialState.js
@@ -0,0 +1,18 @@
+const initialState = {
+    tables: [
+        {
+            id: '1',
+            status: 'busy',
+            peopleAmount: 3,
+            maxPeopleAmount: 4,
+            bill: 45
+        },
+        {
+            id: '2',
+            status: 'busy',
+            peopleAmount: 5,
+            maxPeopleAmount: 6,
+            bill: 93
+        },
+    ]
+}
