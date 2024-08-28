ddfb0f7aabae63b2c8e4b7ed95c7d05b9c5e1edc
dave8git <dave8git@gmail.com>
2024-08-28 18:59:08 +0200

    Add addBookRequest added and used in BooksForm

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,48 @@ const reducer = (state, action) => {
     return state;
 }
 
-const initialState = {};
+const initialState = {
+    tables: [
+        {
+            "tables": [
+              {
+                "id": 1,
+                "status": "Reserved",
+                "peopleAmount": "2",
+                "maxPeopleAmount": "4",
+                "bill": "28",
+                "people": "2",
+                "maxPeople": "4"
+              },
+              {
+                "id": 2,
+                "status": "Busy",
+                "peopleAmount": 0,
+                "maxPeopleAmount": 3,
+                "bill": "118",
+                "people": "5",
+                "maxPeople": 6
+              },
+              {
+                "id": 3,
+                "status": "Busy",
+                "peopleAmount": 2,
+                "maxPeopleAmount": 3,
+                "bill": 45
+              },
+              {
+                "id": 4,
+                "status": "Cleaning",
+                "peopleAmount": 0,
+                "maxPeopleAmount": 3,
+                "bill": 0,
+                "people": 0,
+                "maxPeople": 3
+              }
+            ]
+          }
+    ]
+};
 
 const store = createStore(
     reducer, 
